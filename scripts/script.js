2641dd020742e118b1179fa8a6295662250f91f3
andruuul <andrescanulm@gmail.com>
2022-08-02 16:49:46 -0500

    finish preloading pictures

--- scripts/script.js
+++ scripts/script.js
@@ -38,16 +38,46 @@ function saveInputChanges () {
   closePopupContainer() //*Muchas gracias por todas tus observaciones!
 }
 
+//Cards iniciales
+
+const initialCards = [
+  {  name: "Valle de Yosemite",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg"  },
+  {  name: "Lago Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"  },{  name:"Montañas Calvas", link:"https://code.s3.yandex.net/web-code/bald-mountains.jpg"},
+  {  name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg"  },
+  {  name: "Parque Nacional de la Vanoise",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg"  },
+  {  name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg"  }
+];
+
+//Cargar las Cards iniciales
+
+const elementsGrid = document.querySelector("#elementsGrid");
+
+const cardsTemplate = document.querySelector("#cardTemplate").content;
+for (var i=0;i<initialCards.length;i++) {
+const cardsElement = cardsTemplate.querySelector(".elements-grid__card").cloneNode(true);
+cardsElement.querySelector(".elements-grid__photo").src = initialCards[i].link;
+cardsElement.querySelector(".elements-grid__place-name").textContent = initialCards[i].name;
+elementsGrid.append(cardsElement)
+}
+
+//Desplegar formulario para añadir una tarjeta
+
+
 //*****Para la próxima, leer sobre: "submit", preventDefault, etc.//
 
+//likeBtn 
 
-//likeBtn (todavía no)
+// var likeBtns = document.getElementsByClassName('elements-grid__like-button')
 
-var likeBtns = document.getElementsByClassName('elements-grid__like-button')
+//for (const likeBtn of likeBtns) {
+//  likeBtn.addEventListener('click', activateLikeBtn) 
+//  function activateLikeBtn() {
+// likeBtn.setAttribute('style', 'background: url("./images/like-button-active.svg"); background-repeat: no-repeat;');
+//  };
+//}
 
-for (const likeBtn of likeBtns) {
-  likeBtn.addEventListener('click', activateLikeBtn) 
-  function activateLikeBtn() {
-    likeBtn.setAttribute('style', 'background: url("./images/like-button-active.svg"); background-repeat: no-repeat;');
-  };
-}
