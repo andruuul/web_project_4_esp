eaf7851382825aedc6d1fd251a34c0a4a4e30fae
Andres Canul <andrescanulm@gmail.com>
2022-10-19 17:35:03 -0500

    enviado a christian

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,21 +23,6 @@ function getUserInfoFromServer() {
 }
 getUserInfoFromServer();
 
-const userInfo = new UserInfo(defaultUsername, defaultSubtitle);
-
-const profilePopup = new PopupWithForm ("#popupContainer", ({name, job}) => {
-  userInfo.setUserInfo(name, job);
-})
-
-editBtn.addEventListener("click", () => {
-  const { name, job } = userInfo.getUserInfo();
-  inputUserName.value = name;
-  inputSubtitle.value = job;
-  profilePopup.open()
-  formValidators['profile-form'].resetValidation()
-})
-profilePopup.setEventListeners()
-
 function getCardsFromServer() {
   fetch("https://around.nomoreparties.co/v1/web_es_cohort_02/cards", {
     headers: {
@@ -61,41 +46,7 @@ function getCardsFromServer() {
 }
 getCardsFromServer();
 
-//PATCH
-
-fetch("https://around.nomoreparties.co/v1/web_es_cohort_02/users/me", {
-  method: "PATCH",
-  headers: {
-    authorization: "c0a099b3-69e1-4897-8731-fc3bd1c460e5",
-    "Content-Type": "application/json"
-  },
-  body: JSON.stringify({
-    name: "Marie Skłodowska Curie",
-    about: "Físico y químicos"
-  })
-});
-
-//POST
-
-fetch("https://around.nomoreparties.co/v1/web_es_cohort_02/cards", {
-  method: "POST",
-  headers: {
-    authorization: "c0a099b3-69e1-4897-8731-fc3bd1c460e5",
-    "Content-Type": "application/json"
-  },
-  body: JSON.stringify({
-    name: "nombredelafoto", //cardData.name,
-    link: "Físico y químicos" //cardData.link
-  })
-});
- 
-const newPlacePopup = new PopupWithForm ("#popupContainerNewPlace", (cardData) => {
-  const customCardReady = createCard(cardData)
-  cardsList.addItem(customCardReady); //¿Cómo puedo utilizar cardsList otra vez, si está en un .then de otra función? :( O ya no la necesito?
-});
-
-console.log(newPlacePopup)
-
+const userInfo = new UserInfo(defaultUsername, defaultSubtitle); 
 
 function createCard(item) {
   const card = new Card ({placeName: item.name, photo: item.link, cardTemplateSelector:cardTemplate, callbackImage: (evt) => {
@@ -120,6 +71,25 @@ const enableValidation = (settings) => {
   });
 };
 enableValidation(settings);
+
+
+const profilePopup = new PopupWithForm ("#popupContainer", ({name, job}) => {
+   userInfo.setUserInfo(name, job);
+})
+  
+editBtn.addEventListener("click", () => {
+  const { name, job } = userInfo.getUserInfo();
+  inputUserName.value = name;
+  inputSubtitle.value = job;
+  profilePopup.open()
+  formValidators['profile-form'].resetValidation()
+})
+profilePopup.setEventListeners()
+
+const newPlacePopup = new PopupWithForm ("#popupContainerNewPlace", (cardData) => {
+  const customCardReady = createCard(cardData)
+  cardsList.addItem(customCardReady);
+});
 
 
 addBtn.addEventListener("click", () => {
