9f7b7a2b398d4c0b87199ee030c6cd954896c745
Andres Canul <andrescanulm@gmail.com>
2022-10-19 16:40:47 -0500

    necesito ayuda con PATCH y POST

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import '../styles/normalize.css'
 import Card from '../components/Card.js'
 import {FormValidator} from '../components/FormValidator.js'
 import Section from '../components/Section.js';
-import { initialCards, elementsGridSection, inputSubtitle, inputUserName, cardTemplate, settings, addBtn, editBtn, defaultSubtitle, defaultUsername, profilePicture } from '../utils/constants.js'; 
+import { elementsGridSection, inputSubtitle, inputUserName, cardTemplate, settings, addBtn, editBtn, defaultSubtitle, defaultUsername, profilePicture } from '../utils/constants.js'; 
 import PopupWithForm from '../components/PopupWithForm.js';
 import PopupWithImage from '../components/PopupWithImage.js';
 import UserInfo from '../components/UserInfo.js';
@@ -22,6 +22,21 @@ function getUserInfoFromServer() {
   })
 }
 getUserInfoFromServer();
+
+const userInfo = new UserInfo(defaultUsername, defaultSubtitle);
+
+const profilePopup = new PopupWithForm ("#popupContainer", ({name, job}) => {
+  userInfo.setUserInfo(name, job);
+})
+
+editBtn.addEventListener("click", () => {
+  const { name, job } = userInfo.getUserInfo();
+  inputUserName.value = name;
+  inputSubtitle.value = job;
+  profilePopup.open()
+  formValidators['profile-form'].resetValidation()
+})
+profilePopup.setEventListeners()
 
 function getCardsFromServer() {
   fetch("https://around.nomoreparties.co/v1/web_es_cohort_02/cards", {
@@ -46,7 +61,41 @@ function getCardsFromServer() {
 }
 getCardsFromServer();
 
-const userInfo = new UserInfo(defaultUsername, defaultSubtitle); 
+//PATCH
+
+fetch("https://around.nomoreparties.co/v1/web_es_cohort_02/users/me", {
+  method: "PATCH",
+  headers: {
+    authorization: "c0a099b3-69e1-4897-8731-fc3bd1c460e5",
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify({
+    name: "Marie Skłodowska Curie",
+    about: "Físico y químicos"
+  })
+});
+
+//POST
+
+fetch("https://around.nomoreparties.co/v1/web_es_cohort_02/cards", {
+  method: "POST",
+  headers: {
+    authorization: "c0a099b3-69e1-4897-8731-fc3bd1c460e5",
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify({
+    name: "nombredelafoto", //cardData.name,
+    link: "Físico y químicos" //cardData.link
+  })
+});
+ 
+const newPlacePopup = new PopupWithForm ("#popupContainerNewPlace", (cardData) => {
+  const customCardReady = createCard(cardData)
+  cardsList.addItem(customCardReady); //¿Cómo puedo utilizar cardsList otra vez, si está en un .then de otra función? :( O ya no la necesito?
+});
+
+console.log(newPlacePopup)
+
 
 function createCard(item) {
   const card = new Card ({placeName: item.name, photo: item.link, cardTemplateSelector:cardTemplate, callbackImage: (evt) => {
@@ -71,25 +120,6 @@ const enableValidation = (settings) => {
   });
 };
 enableValidation(settings);
-
-
-const profilePopup = new PopupWithForm ("#popupContainer", ({name, job}) => {
-   userInfo.setUserInfo(name, job);
-})
-  
-editBtn.addEventListener("click", () => {
-  const { name, job } = userInfo.getUserInfo();
-  inputUserName.value = name;
-  inputSubtitle.value = job;
-  profilePopup.open()
-  formValidators['profile-form'].resetValidation()
-})
-profilePopup.setEventListeners()
-
-const newPlacePopup = new PopupWithForm ("#popupContainerNewPlace", (cardData) => {
-  const customCardReady = createCard(cardData)
-  cardsList.addItem(customCardReady);
-});
 
 
 addBtn.addEventListener("click", () => {
